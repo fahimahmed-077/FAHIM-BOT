a89145c32b6731a24391534b3be2439b8dd7edf8
FAHIM AHMED RAJ <fahimahmedraj207@gmail.com>
2026-05-17 18:24:57 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,22 +15,43 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["61584807686126",];
+		const permission = [
+			"61589439339903",
+			"61589976056816"
+		];
+
 		if (!permission.includes(event.senderID)) {
-			return api.sendMessage("𝐤𝐡𝐚𝐧𝐤𝐢𝐫 𝐜𝐡𝐞𝐥𝐞 𝐣𝐚 𝐯𝐚𝐠𝐠𝐠 𝐛𝐨𝐭 𝐤𝐢 𝐭𝐨𝐫 𝐛𝐚𝐩𝐞𝐫...🖕😒", event.threadID, event.messageID);
+			return api.sendMessage(
+				"𝐤𝐡𝐚𝐧𝐤𝐢𝐫 𝐜𝐡𝐞𝐥𝐞 𝐣𝐚 𝐯𝐚𝐠𝐠𝐠 𝐛𝐨𝐭 𝐤𝐢 𝐭𝐨𝐫 𝐛𝐚𝐩𝐞𝐫...🖕😒",
+				event.threadID,
+				event.messageID
+			);
 		}
 
 		const fileName = args[0];
 		if (!fileName) {
-			return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
+			return api.sendMessage(
+				"Please provide a file name.",
+				event.threadID,
+				event.messageID
+			);
 		}
 
 		const filePath = __dirname + `/${fileName}.js`;
+
 		if (!fs.existsSync(filePath)) {
-			return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+			return api.sendMessage(
+				`File not found: ${fileName}.js`,
+				event.threadID,
+				event.messageID
+			);
 		}
 
 		const fileContent = fs.readFileSync(filePath, 'utf8');
-		api.sendMessage({ body: fileContent }, event.threadID);
+
+		api.sendMessage(
+			{ body: fileContent },
+			event.threadID
+		);
 	}
 };
