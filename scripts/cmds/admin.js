df51735f4acefe329168be53ed7a899f365ec814
FAHIM AHMED RAJ <fahimahmedraj207@gmail.com>
2026-06-24 17:59:03 +0600

    Update admin.js

--- scripts/cmds/admin.js
+++ scripts/cmds/admin.js
@@ -47,7 +47,7 @@ module.exports = {
 			case "add":
 			case "-a": {
 				if (!isOwner)
-					return message.reply("❌ | Only my boss farhan can add operator.");
+					return message.reply("❌ | Only my boss fahim can add operator.");
 
 				let uids = [];
 				if (event.type === "message_reply") {
